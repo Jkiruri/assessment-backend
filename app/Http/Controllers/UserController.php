b9e5eac2ad3ac2e0ce41d4337b718a8ec2deb653
James <jamesnjunge45@gmail.com>
2024-06-04 09:51:38 +0300

    update user and album controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $response = Http::get('https://jsonplaceholder.typicode.com/users');
+        return response()->json($response->json());
+    }
+
+    public function show($id)
+    {
+        $response = Http::get("https://jsonplaceholder.typicode.com/users/{$id}");
+        return response()->json($response->json());
+    }
 }
