06aea762108225b4610d4e6cc2356f52df9da907
James <jamesnjunge45@gmail.com>
2024-06-05 08:38:12 +0300

    adding api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('albums', AlbumController::class);
+Route::apiResource('photos', PhotoController::class);
+Route::apiResource('users', UserController::class);
